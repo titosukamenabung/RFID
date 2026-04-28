ba8d90e832dbc0cd7422a3353b9cfe9bb611e054
tito <titosukamenabung@gmail.com>
2026-04-28 18:54:16 +0700

    contoh

--- src/main/java/imam.java
+++ src/main/java/imam.java
@@ -2,12 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.absensi_rfid.objects;
 
 /**
  *
  * @author ADVAN
  */
-public class contoh {
+public class imam {
     
 }
